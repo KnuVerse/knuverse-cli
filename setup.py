f3c76771d4e03ffa75bafc1a9b12e564d3238635
Zachary Sells <zsells@knuverse.com>
2016-10-26 11:26:26 -0700

    First commit. Includes ability to verify via audiopass and audiopin

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='knuverse-cli',
+    version='1.0.2',
+    description='Demo of the Knuverse SDK that lets you verify with AudioPIN and AudioPass',
+    long_description=open('README.md').read(),
+    url='https://github.com/KnuVerse/knuverse-cli',
+    author='KnuVerse',
+    author_email='support@knuverse.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='api sdk knuverse cloud voice authentication audiopin audiopass demo',
+
+    install_requires=['knuverse', 'colorama', 'pyaudio'],
+    packages=find_packages(exclude=['examples']),
+    entry_points={
+        'console_scripts': [
+            'knuverse = cli.__main__:main'
+        ]
+    },
+)
